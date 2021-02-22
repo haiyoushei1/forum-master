7a26d162a7298011d857b7934d89e3bf036758d3
haiyoushei1 <1260404943@qq.com>
2021-02-22 10:43:28 +0800

    first commit

---
+++ src/main/java/com/studycloud1/forummaster/ForumMasterApplication.java
@@ -0,0 +1,13 @@
+package com.studycloud1.forummaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studycloud1/forummaster/ForumMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studycloud1.forummaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
