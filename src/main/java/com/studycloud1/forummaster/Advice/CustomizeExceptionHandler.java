84dd7374e99a21c91cafe6891d9d962e3767abeb
haiyoushei1 <1260404943@qq.com>
2021-03-11 22:16:48 +0800

    exception hanler

---
+++ src/main/java/com/studycloud1/forummaster/Advice/CustomizeExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.studycloud1.forummaster.Advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class CustomizeExceptionHandler {
+    @ExceptionHandler
+    public ModelAndView handle(HttpServletRequest request, Throwable ex, Model model){
+
+        HttpStatus status = getStatus(request);
+        model.addAttribute("message", "资源走丢了");
+        return new ModelAndView("error");
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request){
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if(statusCode == null){
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
+    }
+}
